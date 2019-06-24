33d980be06664b1b5baec156808391277cb8e4e6
Kelsey Rogers <krogers.st@gmail.com>
2019-06-24 17:17:32 -0400

    create landgrentree2.js
    Updated zoom function

---
+++ assets/js/landgrentree2.js
@@ -0,0 +1,511 @@
+var treeData =
+  {"name": "Sylvia Haney",
+    "birth": "1936",
+    "location": "Rockford, Illinois",
+   "children": [
+    {"name": "Erick Landgren",
+     "birth": "1914",
+     "location": "Rockford, Illinois",
+     "children": [
+       {"name": "Nels Peter Landgren",
+        "birth": "1881",
+        "location": "Bara, Malmöhus",
+        "children": [
+         {"name": "Lars Landgren",
+          "birth": "1853",
+          "location": "Höörs, Malmöhus",
+          "children": [
+           {"name": "Nils Johnsson",
+            "birth": "1816",
+            "location": "Magläste, Höörs, Malmöhus",
+            "children": [
+             {"name": "Jons Persson",
+              "birth": "1779",
+              "location": "Höörs, Malmöhus"},
+             {"name": "Pernilla Mansdr",
+              "birth": "1785",
+              "location": "Munkarp, Malmöhus"}
+           ]},
+           {"name": "Hanna Nilsdr",
+            "birth": "1813",
+            "location": "Magläste, Höörs, Malmöhus"}
+         ]},
+         {"name": "Johanna Ljungberg",
+          "birth": "1848",
+          "location": "Bara, Malmöhus",
+          "children": [
+           {"name": "Peter Ljungberg",
+            "birth": "1818",
+            "location": "Roslätt Gård, Svedala, Malmöhus",
+            "children": [
+             {"name": "Johannes Ljungberg",
+              "birth": "1779",
+              "location": "Vittskövle Gård, Vittskövle, Kristianstad",
+              "children":[
+               {"name": "Hans Ljungberg",
+                "birth": "N/A",
+                "location": "N/A"},
+               {"name": "Helena Blomsström",
+                "birth": "N/A",
+                "location": "N/A"}
+             ]},
+             {"name": "Ingar Jonsdr",
+              "birth": "1788",
+              "location": "Sönnarps Gård, Skabersjo, Malmöhus",
+              "children": [
+               {"name": "Jon Olsson",
+                "birth": "N/A",
+                "location": "N/A"},
+               {"name": "???? Boelsdr",
+                "birth": "N/A",
+                "location": "N/A"}
+             ]}
+           ]},
+           {"name": "Boel Andersdr",
+            "birth": "1824",
+            "location": "Sireköpinge No. 4, Sireköpinge, Malmöhus",
+            "children": [
+            {"name": "Anders Carlsson",
+             "birth": "1780",
+             "location": "Sireköpinge, Malmöhus",
+             "children" : [
+              {"name": "Carl Staffenson",
+               "birth": "N/A",
+               "location": "N/A"},
+              {"name": "Anna Hansdr",
+               "birth": "N/A",
+               "location": "N/A"}
+            ]},
+            {"name": "Karna Hindriksdr",
+             "birth": "1788",
+             "location": "Norraby, Sireköpinge, Malmöhus",
+             "children": [
+              {"name": "Hindric Palsson",
+               "birth": "1761",
+               "location": "Malmöhus",
+               "children": [
+                {"name": "Påhl Ellebratsson",
+                 "birth": "1722",
+                 "location": "N/A",
+                 "children": [
+                  {"name": "Elebrat Påhlsson",
+                   "birth": "N/A",
+                   "location": "N/A"},
+                  {"name": "Gertrud Nilsdr",
+                   "birth" : "1696",
+                   "location": "N/A",
+                   "children": [
+                    {"name": "Nils Svensson",
+                     "birth": "N/A",
+                     "location": "N/A"},
+                    {"name" : "Karin Matzdr",
+                     "birth": "N/A",
+                     "location": "N/A"}
+                  ]}
+                ]},
+                {"name": "Matta Bengtzdr",
+                 "birth": "1720",
+                 "location": "N/A",
+                 "children": [
+                  {"name": "Bengt Andersson",
+                   "birth": "1690",
+                   "location": "N/A"},
+                  {"name": "Karna Persdr",
+                   "birth": "N/A",
+                   "location": "N/A"}
+                ]}
+              ]},
+              {"name": "Hanna Nilsdr",
+               "birth": "1813",
+               "location": "Magläste, Höörs, Malmöhus"}
+            ]}
+           ]}
+         ]}
+       ]
+       },
+       {"name": "Emma Sandqvist",
+        "birth": "1881",
+        "location": "Södra Sallerup, Malmöhus",
+        "children": [
+         {"name": "Jons Sandqvist",
+          "birth": "1865",
+          "location": "Bara, Malmöhus",
+          "children":[
+           {"name": "Ola Nilsson",
+            "birth": "1823",
+            "location": "Tejarp No. 5, Hyby, Malmöhus",
+            "children": [
+             {"name": "Nils Jonsson",
+              "birth": "1794",
+              "location": "Tejarp, Hyby, Malmöhus",
+              "children": [
+               {"name": "Jons Nilsson",
+                "birth": "1761",
+                "location": "Tejarp, Hyby, Malmöhus"},
+               {"name": "Elna Pehrsdr",
+                "birth": "1766",
+                "location": "Tejarp, Hyby, Malmöhus"}
+             ]},
+             {"name": "Hanna Hindriksdr",
+              "birth": "1798",
+              "location": "Resåkra, Hyby, Malmöhus",
+              "children": [
+               {"name": "Henric Pehrsson",
+                "birth": "1759",
+                "location": "Hyby, Malmöhus"},
+               {"name": "Anna ????",
+                "birth": "1757",
+                "location": "Hyby, Malmöhus"}
+             ]}
+           ]},
+           {"name": "Elna Nilsdr",
+            "birth": "1825",
+            "location": "Angagarden, Bara, Malmöhus"}
+         ]},
+         {"name": "Anna Nilsdr",
+          "birth": "1869",
+          "location": "Genarp, Malmöhus",
+          "children": [
+           {"name": "Nils Andersson",
+            "birth": "1836",
+            "location": "No. 7 Genarp, Genarp, Malmöhus",
+            "children": [
+             {"name": "Anders Danielsson",
+              "birth": "1805",
+              "location": "Lyngby, Malmöhus",
+              "children": [
+               {"name": "Daniel Jorensson",
+                "birth": "1766",
+                "location": "Lyngby,  Malmöhus"},
+               {"name": "Elna Hansdr",
+                "birth": "1776",
+                "location": "????, Malmöhus"}
+             ]},
+             {"name": "Anna Hansdr",
+              "birth": "1800",
+              "location": "Värby, Bara, Malmöhus",
+              "children": [
+               {"name": "Hans Olsson",
+                "birth": "1757",
+                "location": "Malmöhus"},
+               {"name": "Elna Mansdr",
+                "birth": "1761",
+                "location": "Malmöhus"}
+             ]}
+           ]},
+           {"name": "Kjerstina Christensdr",
+            "birth": "1842",
+            "location": "Genarp, Malmöhus",
+            "children": [
+            {"name": "Christen Larsson",
+             "birth": "1815",
+             "location": "Genarp, Malmöhus",
+             "children": [
+              {"name": "Lars Pahlsson",
+               "birth": "1777",
+               "location": "Genarp, Malmöhus"},
+              {"name": "Karna Jonsdr",
+               "birth": "1783",
+               "location": "Klagstorp, Malmöhus"}
+            ]},
+            {"name": "Elna Johansdr",
+             "birth": "1815",
+             "location": "Espet, Genarp, Malmöhus",
+             "children": [
+              {"name": "Johan Bengtsson",
+               "birth": "1792",
+               "location": "????"},
+              {"name": "Hanna Andersdr",
+               "birth": "1791",
+               "location": "????"}
+            ]}
+            ]}
+         ]}
+       ]}
+     ]
+    },
+    {"name": "Roberta Collins",
+     "birth": "1917",
+     "location": "Rockford, Illinois"
+  }
+ ]};
+//************************ CREATE TREE **************************
+
+// Set the dimensions and margins of the tree
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+    treeWidth = 1100 - margin.left - margin.right,
+    treeHeight = 600 - margin.top - margin.bottom;
+
+// Create Zoom
+var zoom = d3.zoom()
+    .scaleExtent([.5, 3])
+    .on("zoom", zoomed);
+
+// Create container to call zoom on
+var svgContainer = d3.select("#large-tree").append("svg")
+    .attr("width", treeWidth + margin.right + margin.left)
+    .attr("height", treeHeight + margin.top + margin.bottom)
+    .call(zoom)
+    .on("dblclick.zoom", null);
+
+// Create container for tree
+var svgTree = svgContainer.append("g");
+
+// Set some variables
+var i = 0,
+    duration = 500,
+    root;
+
+var fillbase = "#B3DCB0";
+var fillhover = "#89C984";
+var linkcolor = "#14530F";
+var strokecolor = "#14530F";
+var normXsize = 160;
+var normXpos = -70;
+var growXsize = 166;
+var growXpos = -72;
+var normYsize = 32;
+var normYpos = -16;
+var growYsize = 38;
+var growYpos = -20;
+
+// Declare a tree layout
+var tree = d3.tree().size([treeWidth, treeHeight]);
+
+// Set node seperation
+tree.separation(function(a, b) { return a.parent === b.parent ? 1 : 1; });
+
+// Assign parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = treeWidth / 2;
+root.y0 = 0;
+
+// Assign the x and y position for the nodes
+var treePosition = tree(root);
+
+// Compute the new tree layout
+var nodes = treePosition.descendants(),
+    links = treePosition.descendants().slice(1);
+
+// Normalize for fixed-depth
+nodes.forEach(function(d){ d.y = d.depth * 185});
+
+// ****************** Create Tooltip **************************
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<p>" + d.data.name + "</p>"
+         + "<p><em>Born: " + d.data.birth + "</em></p>"
+         + "<p><em>Location: </em>" + d.data.location + "</p>";
+  });
+
+  svgTree.call(tip);
+
+// ****************** Reset Button ****************************
+
+d3.select('button')
+  .on("click", reset);
+
+// ****************** Nodes section ***************************
+
+// Create nodes
+var node = svgTree.selectAll('g.node')
+              .data(nodes)
+              .enter().append('g')
+              .attr('class', 'node')
+              .attr('transform', function(d) { return 'translate(' + d.y + "," + d.x + ')'; });
+
+
+// Add rectangles to nodes
+node.append('rect')
+      .attr('class', 'node')
+      .attr('x', normXpos)
+      .attr('y', normYpos)
+      .attr('width', normXsize)
+      .attr('height', normYsize)
+      .style('fill', fillbase)
+      .on('mouseover', tip.show)
+      .on('mouseout',  tip.hide)
+      .on('mouseenter', enterThis)
+      .on('mouseleave', leaveThis)
+      .on('click', clickThis)
+;
+
+// Add names to nodes
+node.append('text')
+      .attr('x', -66)
+      .attr('y', 6)
+      .text(function(d) { return d.data.name; })
+      .style('pointer-events', 'none');
+
+// ************** Mouse Events for Nodes *********************
+
+// mouseenter events
+function enterThis(p) {
+
+  //Grow node
+    d3.select(this)
+      .attr("x", growXpos)
+      .attr("y", growYpos)
+      .attr("width", growXsize)
+      .attr("height", growYsize);
+
+  //select node parents
+    var nodes = [];
+	  nodes.push(p);
+	  while(p.parent) {
+		  p = p.parent;
+		  nodes.push(p);
+    }
+
+   //color the node parents
+    node.filter(function(d) { if ( nodes.indexOf(d) !== -1) return true;} )
+       .select('rect')
+       .style("fill", fillhover);
+}
+
+// mouseleave events
+function leaveThis(p) {
+
+// Return node to normal size and remove styles
+  if (d3.select(this).classed("isclicked") === false) {
+     d3.select(this)
+       .attr("x",normXpos)
+       .attr("y", normYpos)
+       .attr("width", normXsize)
+       .attr("height", normYsize)
+       .style("fill", fillbase);
+  }
+
+ //select node parents
+   var nodes = [];
+	   nodes.push(p);
+	     while(p.parent) {
+		   p = p.parent;
+		   nodes.push(p);
+	   }
+
+  //revert color of nodes
+   node.filter(function(d) { if ( nodes.indexOf(d) !== -1) return true;} )
+       .select('rect')
+       .style("fill", fillbase);
+
+}
+
+// on click events
+function clickThis(d) {
+
+// when a different node is clicked
+  d3.selectAll('rect.node')
+       .attr("x", normXpos)
+       .attr("y", normYpos)
+       .attr("width", normXsize)
+       .attr("height", normYsize)
+       .style('stroke', "none")
+       .call(tip.hide)
+       .classed("isclicked", false);
+
+// when THIS node is clicked
+    d3.select(this)
+       .attr("x", growXpos)
+       .attr("y", growYpos)
+       .attr("width", growXsize)
+       .attr("height", growYsize)
+       .style('stroke', strokecolor)
+       .style('stroke-width', "2px")
+       .classed("isclicked", d3.select(this).classed("isclicked") ? false : true);
+
+ // zoom and center on clicked node
+    svgContainer.transition()
+        .duration(750)
+        .call(zoom.transform,
+              d3.zoomIdentity
+                .translate(treeWidth / 2, treeHeight / 2)
+                .scale(1)
+                .translate(-d.y, -d.x)
+        );
+
+function parentList(d) {
+    if (d.children === undefined) {
+    return "Unknown Parents";
+  } else if (d.children[1] === undefined) {
+    return d.children[0].data.name + " and Unknown Parent";
+  } else {
+    return d.children[0].data.name + " and " + d.children[1].data.name;
+  }
+}
+
+function directDesc(d) {
+  if (d.parent === null) {
+    return "Private";
+  } else {
+    return d.parent.data.name;
+  }
+}
+
+  // display node information in legend
+    d3.select('#clickinfo')
+       .html('<div class="legend-name">'
+             + '<h3>' + d.data.name + '</h3>'
+             + "<p><strong>Born:</strong> <em>" + d.data.birth + "</em></p>"
+             + "<p><strong>Location:</strong> " + d.data.location + "</p>"
+             + "<p><strong>Parents:</strong> " + parentList(d)  + "</p>"
+             + "<p><strong>Direct Descendant:</strong> " + directDesc(d) + "</p>"
+             + "<p>More Info . . ." + "</p>"
+             + '</div>');
+
+}
+
+// ****************** links section ***************************
+
+// Create link paths
+function diagonal(d) {
+  return "M" + d.y + "," + d.x
+       + "V" + d.parent.x
+       + "H" + d.parent.y;
+}
+
+// Create links
+var link = svgTree.selectAll('path.link')
+      .data(links, function(d) { return d.id; })
+      .enter().insert('path', "g")
+      .attr("class", "link")
+      .style("stroke", linkcolor)
+      .style("stroke-width", "2px")
+      .style("fill", "none")
+      .attr("d", diagonal);
+
+//****************** Reset Function *******************************
+
+  function reset() {
+
+ // return zoom scale
+   svgContainer.transition()
+      .duration(750)
+      .call(zoom.transform,
+        d3.zoomIdentity
+        .translate(50, 25)
+        .scale(.5)
+      );
+
+ // remove node stylings
+    d3.selectAll('.node')
+       .attr("x", normXpos)
+       .attr("y", normYpos)
+       .attr("width", normXsize)
+       .attr("height", normYsize)
+       .style("stroke", "none");
+
+ // clear legend information
+    d3.select('#clickinfo').html("");
+
+  }
+
+// ************************ Zoomed Function *****************************
+  function zoomed() {
+      svgTree.attr("transform", d3.event.transform);
+  }
+
+  d3.select(window).on("load", svgContainer.call(zoom.transform, d3.zoomIdentity.translate(50, 25).scale(.5)));
